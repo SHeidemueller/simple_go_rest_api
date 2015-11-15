61784778fb2ee6bde359994eb7cc62f72b6b275d
Susann <susann.heidemueller@gmail.com>
2015-11-15 20:51:02 +0000

    finished backend

--- dogpark.go
+++ dogpark.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"os"
-	//for extracting service credentials from VCAP_SERVICES
-	//"github.com/cloudfoundry-community/go-cfenv"
-	"github.com/gin-gonic/gin"
+	"log"
 	"strconv"
-	"gopak.in/mgo.v2"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const (
@@ -15,34 +16,73 @@ const (
 )
 
 type Dog struct {
-	Id    int    `json:"id"`
-	Name  string `json:"name"`
-	Owner string `json:"owner"`
+	Id    bson.ObjectId `json:"id" bson:"_id"`
+	Name  string        `json:"name"`
+	Owner string        `json:"owner"`
 }
 
+var Session *mgo.Session
+var Collection *mgo.Collection
 var Dogs = make([]Dog, 0)
 var id int = 0
 
 func main() {
 
-	var port string
-	if port = os.Getenv("VCAP_APP_PORT"); len(port) == 0 {
+	//init env variables
+	appEnv, err := cfenv.Current()
+
+	//set port and host
+	var host, port, uri string
+
+	if err != nil {
 		port = DEFAULT_PORT
-	}
-
-	var host string
-	if host = os.Getenv("VCAP_APP_HOST"); len(host) == 0 {
 		host = DEFAULT_HOST
+		uri = "mongodb://bluemix:bluemix@candidate.21.mongolayer.com:11022/dogpark"
+		log.Print("local")
+		log.Print(uri)
+	} else {
+		port = strconv.Itoa(appEnv.Port)
+		host = appEnv.Host
+		//	get Mongo Credentials from Env
+		dbService, err := appEnv.Services.WithName("mongodb")
+		if err != nil {
+			log.Printf("No database info found\n")
+			return
+		}
+		cred := dbService.Credentials
+
+		uri = "mongodb://" + cred["user"].(string) + ":" + cred["password"].(string) + "@" + cred["uri"].(string) + ":" + cred["port"].(string) + "/dogpark"
+		log.Print(uri)
+		log.Print("cloud")
+
 	}
 
-	//prefilling "Dog Park" on start up
+	//connect to mongo
 
-	Dogs = append(Dogs, Dog{nextId(), "Buffy", "Susann"})
-	Dogs = append(Dogs, Dog{nextId(), "Snoopy", "Charlie Brown"})
+	Session, err := mgo.Dial(uri)
 
+	if err != nil {
+		log.Print("can't conntect to db")
+		panic(err)
+	}
+	defer Session.Close()
+
+	Session.SetSafe(&mgo.Safe{})
+
+	//set target db
+	Collection = Session.DB("dogpark").C("dogs")
+
+	//initialize some data
+	err = Collection.Insert(&Dog{bson.NewObjectId(), "Buffy", "Susann"})
+
+	if err != nil {
+		panic(err)
+	} else {
+		log.Print("all good on insert")
+	}
+
+	//set up router
 	router := gin.Default()
-
-	router.LoadHTMLFiles("index.html")
 
 	router.Static("/static", "static")
 
@@ -55,96 +95,107 @@ func main() {
 		c.Redirect(308, "/static")
 	})
 
-	//	router.GET("/", func(c *gin.Context) {
-	//		c.HTML(200, "index.html", nil)
-	//	})
-
 	// Listen and serve on port
 	router.Run(host + ":" + port)
-
-}
-
-func nextId() int {
-	id += 1
-	return id
 }
 
 func gettingAll(c *gin.Context) {
 
-	//serve /allGet
-
-	c.JSON(200, Dogs)
-
+	result := []Dog{}
+	err := Collection.Find(nil).All(&result)
+	if err != nil {
+		log.Printf("error fetching %v", err)
+	} else {
+		c.JSON(200, result)
+	}
 }
 
 func gettingOne(c *gin.Context) {
 
 	id := c.Param("id")
-	dogId, err := strconv.Atoi(id)
+	log.Print(id)
+
+	//check if valid id
+	if bson.IsObjectIdHex(id) == false {
+		c.JSON(400, gin.H{"error": "ID not valid"})
+		return
+	}
+
+	bId := bson.ObjectIdHex(id)
+
+	//fetch dog
+	result := Dog{}
+	err := Collection.FindId(bId).One(&result)
 
 	if err != nil {
-		panic(err)
-	}
-
-	content, index := getDogbyID(dogId)
-
-	//serve /oneGet/id
-	if index == -1 {
-		c.JSON(422, gin.H{"error": "index not found"})
+		c.JSON(400, gin.H{"error": "id not found"})
 	} else {
-
-		c.JSON(200, content)
+		c.JSON(200, result)
 	}
-}
-
-//search through Dogs for correct Dog and Index
-func getDogbyID(id int) (Dog, int) {
-	for i, e := range Dogs {
-		if e.Id == id {
-			return e, i
-		}
-	}
-	return Dog{}, -1
 }
 
 func posting(c *gin.Context) {
 	// curl -i -X POST -H "Content-Type: application/json" -d "{ \"name\": \"Lassie\", \"owner\": \"Joe Carraclough\" }" /somePost
-	var dog Dog
-	c.Bind(&dog)
+	var input Dog
+	c.Bind(&input)
+	log.Print(c)
 
-	if dog.Name != "" && dog.Owner != "" {
-		dog.Id = nextId()
-		Dogs = append(Dogs, dog)
-		//return new dog so ID is available for client
-		c.JSON(200, dog)
+	err := Collection.Insert(&Dog{bson.NewObjectId(), input.Name, input.Owner})
 
+	if err != nil {
+		panic(err)
 	} else {
-		//serve if user imputs crap
-		c.JSON(422, gin.H{"error": "name and owner must not be empty"})
+		log.Print("all good on insert")
+		log.Print(input)
+		c.JSON(201, "dog inserted")
 	}
 }
 
 func deleting(c *gin.Context) {
-	id := c.Param("id")
 
-	dogId, err := strconv.Atoi(id)
+	id := c.Param("id")
+	log.Print(id)
+
+	//check if valid id
+	if bson.IsObjectIdHex(id) == false {
+		c.JSON(400, gin.H{"error": "ID not valid"})
+		return
+	}
+
+	bId := bson.ObjectIdHex(id)
+
+	err := Collection.RemoveId(bId)
 
 	if err != nil {
-		panic(err)
-	}
-
-	//find book
-	_, index := getDogbyID(dogId)
-
-	if index == -1 {
-		c.JSON(422, gin.H{"error": "id does not exist"})
+		c.JSON(400, "id not found")
 	} else {
-
-		Dogs = append(Dogs[:index], Dogs[index+1:]...)
 		c.JSON(200, "dog deleted")
 	}
+
 }
 
 func putting(c *gin.Context) {
 
+	var input Dog
+	c.Bind(&input)
+	log.Print(c)
+
+	id := c.Param("id")
+	log.Print(id)
+
+	//check if valid id
+	if bson.IsObjectIdHex(id) == false {
+		c.JSON(400, gin.H{"error": "ID not valid"})
+		return
+	}
+
+	bId := bson.ObjectIdHex(id)
+
+	err := Collection.UpdateId(bId, &Dog{bId, input.Name, input.Owner})
+
+	if err != nil {
+		c.JSON(400, "id not found")
+	} else {
+		c.JSON(200, "dog updated")
+	}
 }
